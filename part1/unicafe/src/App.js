fd3c0b0029da50789fe6ab7854d8611dc10d1ac3
juvester <juvester@users.noreply.github.com>
2023-07-26 16:09:16 +0300

    Add exercise 1.11*: unicafe step6
    Display the statistics in an HTML table.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,7 +4,10 @@ const Button = ({ handleClick, text }) =>
   <button onClick={handleClick}>{text}</button>
 
 const StatisticLine = ({ text, value }) =>
-  <div>{text} {value}</div>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
@@ -15,14 +18,16 @@ const Statistics = ({ good, neutral, bad }) => {
     return <div>No feedback given</div>
 
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive + " %"} />
+      </tbody>
+    </table>
   )
 }
 
