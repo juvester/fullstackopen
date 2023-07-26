fbbfafeb4294cb32322e66c0e9317415906c1d64
juvester <juvester@users.noreply.github.com>
2023-07-26 15:24:13 +0300

    Add exercise 1.8: unicafe step3
    Refactor the statistics into its own component.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,13 +3,28 @@ import { useState } from "react"
 const Button = ({ handleClick, text }) =>
   <button onClick={handleClick}>{text}</button>
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all || 0
+  const positive = (good / all) * 100 || 0
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = (good / all) * 100
 
   return (
     <div>
@@ -17,14 +32,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text='good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
-
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
