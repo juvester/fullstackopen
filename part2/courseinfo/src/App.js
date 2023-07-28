bc571fec32010f6c5904ebd4b650e0d6fc5c674d
juvester <juvester@users.noreply.github.com>
2023-07-28 17:13:29 +0300

    Add exercise 2.4: Course information step9
    Extend the application to allow for an arbitrary number of courses.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,45 +1,77 @@
-const Header = ({ name }) => <h1>{name}</h1>
+const Header = ({ name }) => <h2>{name}</h2>
 
-const Total = ({ sum }) => <strong>total of {sum} exercises</strong>
+const Total = ({ sum }) => <strong>Total of {sum} exercises</strong>
 
 const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Content = ({ parts }) =>
-  parts.map(part =>
-    <Part key={part.id} part={part} />
-  )
+  <div>
+    {parts.map(part =>
+      <Part key={part.id} part={part} />
+    )}
+  </div>
 
 const Course = ({ course }) =>
-  <>
+  <div>
     <Header name={course.name} />
     <Content parts={course.parts} />
     <Total sum={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
-  </>
+  </div>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course course={course} key={course.id} />
+      )}
+    </>
+  )
 }
 
 export default App
