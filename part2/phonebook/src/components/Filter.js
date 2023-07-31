9bf19ddc9c94983ed56bcf082b5795b791f44f67
juvester <juvester@users.noreply.github.com>
2023-07-31 19:16:07 +0300

    Add exercise 2.10: The Phonebook Step5
    Refactor Filter, PersonForm, and Persons into their own components.

---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,8 @@
+const Filter = ({ onChange }) => (
+  <div>
+    Filter by name:
+    <input type='text' onChange={onChange} />
+  </div>
+)
+
+export default Filter
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,7 @@
+const Persons = ({ persons }) => (
+  persons.map(person =>
+    <div key={person.name}>{person.name} {person.number}</div>
+  )
+)
+
+export default Persons
