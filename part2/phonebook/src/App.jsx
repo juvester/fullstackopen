9cc605386876acdeeec304234acf3dd0293e4ba0
juvester <juvester@users.noreply.github.com>
2023-11-17 18:48:11 +0200

    Vite refactor: Exercise 2.9*

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -28,9 +36,18 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter by name:
+        <input type='text' onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new person</h2>
       <form onSubmit={handleSubmit}>
         <div>
           Name:
@@ -45,7 +62,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {personsToShow.map(person =>
         <div key={person.name}>{person.name} {person.number}</div>
       )}
     </div>
